8537dad296306c696ce345e94491cd95e5c0c005
Claudiane2436 <101910551+Claudiane2436@users.noreply.github.com>
2022-08-06 03:39:48 -0300

    Delete ADS/Banco de dados directory

--- ADS/Banco de dados/confirmar.php
+++
@@ -1,7 +0,0 @@
-<?php 
- $id = filter_input(INPUT_GET, 'codigo', FILTER_SANITIZE_NUMBER_INT);
- echo "Deseja realmente encluir o registro!";
- echo "<a href='excluir.php?id=$id'>Sim |</a>";
- echo "<a href='listagem2.php'>Não</a>";
-
-?>
--- ADS/Banco de dados/crud.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host = 'localhost';
-$basaname = 'exemplo';
-$user = 'root';
-$pass = '';
-
-$conexao = new PDO('mysql:host='.$host.';dbname='.$basaname.'',$user,$pass);
-
-?>
