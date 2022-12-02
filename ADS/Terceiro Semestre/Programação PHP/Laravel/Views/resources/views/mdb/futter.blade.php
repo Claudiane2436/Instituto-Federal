aed7564264b8d44bee7d52534f3a84bf39b13196
Claudiane <101910551+Claudiane2436@users.noreply.github.com>
2022-12-01 21:15:47 -0300

    Add files via upload

---
+++ ADS/Terceiro Semestre/Programação PHP/Laravel/Views/resources/views/mdb/futter.blade.php
@@ -0,0 +1,38 @@
+      <footer class="page-footer font-small " style="background-color:#151414">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12 py-5">
+                  <div class="mb-5 flex-center">
+                      <a class="fb-ic">
+                          <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+                              
+                      <a class="tw-ic">
+                        <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+
+                      <a class="ins-ic">
+                        <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                      </a>
+
+                      <a class="ins-ic">
+                          <i class="fab fa-whatsapp fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+                      </a>
+                  </div>
+              </div>
+            </div>
+          </div>
+
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+          <a href="/"> Custome Car</a>
+        </div>
+
+
+      </footer>
+  </body>
+  <script type="text/javascript" src="mdb/js/jquery.min.js"></script>
+  <script type="text/javascript" src="mdb/js/popper.min.js"></script>
+  <script type="text/javascript" src="mdb/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="mdb/js/mdb.min.js"></script>
+  <script type="text/javascript"></script>
+</html>
