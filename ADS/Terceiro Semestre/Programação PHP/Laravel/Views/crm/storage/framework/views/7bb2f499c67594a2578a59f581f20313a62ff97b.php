c2ec21eed8a67bb2bba992e97e40ad45e1584676
Claudiane <101910551+Claudiane2436@users.noreply.github.com>
2022-12-01 21:08:40 -0300

    Delete ADS/Terceiro Semestre/Programação PHP/Laravel/Views/crm directory

--- ADS/Terceiro Semestre/Programação PHP/Laravel/Views/crm/storage/framework/views/7bb2f499c67594a2578a59f581f20313a62ff97b.php
+++
@@ -1,6 +0,0 @@
-<?php echo $__env->make('mdb.cabecalho', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
-
-<?php echo $__env->yieldContent('content'); ?>;
-  
-<?php echo $__env->make('mdb.futter', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
-<?php /**PATH C:\Users\Aluno\Desktop\aula\crm\resources\views/mdb/mdb-fullpage.blade.php ENDPATH**/ ?>
